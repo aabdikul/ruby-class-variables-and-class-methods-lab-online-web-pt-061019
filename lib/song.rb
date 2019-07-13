18e758f0cdf1259bdbaa13ec5552bfa10453b9b9
Aida Abdikulova <abdikulova@gmail.com>
2019-07-13 04:54:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,6 +9,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
+    @@artists
   end
 
   def self.count
@@ -16,7 +17,7 @@ class Song
   end
 
   def artists
-    return @@artists.uniq
+    @@artists
   end
 
 end
