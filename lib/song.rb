81494aba9465c9d911555324b20a9de53920e89c
Aida Abdikulova <abdikulova@gmail.com>
2019-07-13 04:44:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,8 @@
 class Song
+
   attr_accessor :name, :artist, :genre
 
-  def initialize(name, artist, genre)
+  def new(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
